988f23c6b7ea23d62f3236e60814fd484f368789
logiangvuong225-20 <68698013+logiangvuong225-20@users.noreply.github.com>
2021-01-07 21:13:03 +0700

    update module

---
+++ 25/25.php
@@ -0,0 +1,53 @@
+<?php
+$url_host = 'http://' . $_SERVER['HTTP_HOST'];
+$pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
+$pattern_uri = '/' . $pattern_document_root . '(.*)$/';
+
+preg_match_all($pattern_uri, __DIR__, $matches);
+$url_path = $url_host . $matches[1][0];
+$url_path = str_replace('\\', '/', $url_path);
+
+if (!class_exists('lessc')) {
+    $dir_block = dirname($_SERVER['SCRIPT_FILENAME']);
+    require_once($dir_block . '/libs/lessc.inc.php');
+}
+
+$less = new lessc;
+
+$less->compileFile('less/s1.less', 'css/s1.css');
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>139</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"
+        type="text/css" />
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet"
+        type="text/css" />
+
+    <link href="<?php echo $url_path ?>/css/s1.css" rel="stylesheet" type="text/css" />
+    <?php
+        if (!class_exists('lessc')) {
+            include ('./libs/lessc.inc.php');
+        }
+        $less = new lessc;
+      
+        $less->compileFile('less/s1.less', 'css/s1.css');
+    
+        ?>
+</head>
+
+<body>
+    <br>
+    <?php include 's1-content.php'; ?>
+
+
+
+</body>
+
+</html>
---
+++ 45/45.php
@@ -0,0 +1,55 @@
+<?php
+$url_host = 'http://' . $_SERVER['HTTP_HOST'];
+$pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
+$pattern_uri = '/' . $pattern_document_root . '(.*)$/';
+
+preg_match_all($pattern_uri, __DIR__, $matches);
+$url_path = $url_host . $matches[1][0];
+$url_path = str_replace('\\', '/', $url_path);
+
+if (!class_exists('lessc')) {
+    $dir_block = dirname($_SERVER['SCRIPT_FILENAME']);
+    require_once($dir_block . '/libs/lessc.inc.php');
+}
+
+$less = new lessc;
+
+$less->compileFile('less/check-out-9.less', 'css/check-out-9.css');
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>139</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"
+        type="text/css" />
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet"
+        type="text/css" />
+
+    <link href="<?php echo $url_path ?>/css/check-out-9.css" rel="stylesheet" type="text/css" />
+
+
+    <?php
+        if (!class_exists('lessc')) {
+            include ('./libs/lessc.inc.php');
+        }
+        $less = new lessc;
+     
+        $less->compileFile('less/check-out-9.less', 'css/check-out-9.css');
+   
+        ?>
+</head>
+
+<body>
+    <br>
+    <?php include 'check-out-9-content.php'; ?>
+
+
+
+</body>
+
+</html>
