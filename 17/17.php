c9ef06f00875824f35899f6794926e7ad5cb419d
logiangvuong225-20 <68698013+logiangvuong225-20@users.noreply.github.com>
2021-01-07 18:03:35 +0700

    update module

---
+++ 17/17.php
@@ -0,0 +1,71 @@
+<?php
+$url_host = 'http://' . $_SERVER['HTTP_HOST'];
+$pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
+$pattern_uri = '/' . $pattern_document_root . '(.*)$/';
+
+preg_match_all($pattern_uri, __DIR__, $matches);
+$url_path = $url_host . $matches[1][0];
+$url_path = str_replace('\\', '/', $url_path);
+
+if (!class_exists('lessc')) {
+    $dir_block = dirname($_SERVER['SCRIPT_FILENAME']);
+    require_once($dir_block . '/libs/lessc.inc.php');
+}
+
+$less = new lessc;
+$less->compileFile('less/m1.less', 'css/m1.css');
+
+
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>2</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+
+    <link href="<?php echo $url_path ?>/css/m1.css" rel="stylesheet" type="text/css" />
+
+
+
+
+    <script src='https://code.jquery.com/jquery-3.3.1.js'></script>
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/mixitup/2.1.11/jquery.mixitup.min.js'></script>
+    <script src='https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js'></script>
+    <script
+        src='https://cdn2.hubspot.net/hub/322787/hub_generated/style_manager/1440007714979/custom/page/hack-a-thon-3/masonry.min.min.js'>
+    </script>
+    <script
+        src='https://cdn2.hubspot.net/hub/322787/hub_generated/style_manager/1440007849180/custom/page/hack-a-thon-3/isotope.min.js'>
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+    <script src="./js/g1.js"></script>
+
+    <?php
+        if (!class_exists('lessc')) {
+            include ('./libs/lessc.inc.php');
+        }
+        $less = new lessc;
+  
+
+        ?>
+</head>
+
+<body>
+    <?php include './m1-content.php'; ?>
+
+
+
+
+</body>
+
+</html>
---
+++ 8/8-content.php
@@ -0,0 +1,113 @@
+<div class="module-g1">
+
+    <!-- partial:index.partial.html -->
+    <div class="button-group filters-button-group">
+        <button class="button is-checked" data-filter="*">All</button>
+        <button class="button" data-filter=".beach">Albanian roach</button>
+        <button class="button" data-filter=".bird">Algae eater</button>
+        <button class="button" data-filter=".boat">Armoured catfish</button>
+        <button class="button" data-filter=".lighthouse">Asian carp</button>
+        <button class="button" data-filter=".people">Billfish</button>
+        <!-- <button class="button" data-filter=".rock">rock</button>
+        <button class="button" data-filter=".sea">sea</button> -->
+    </div>
+
+    <section id="grid-container" class="transitions-enabled fluid masonry js-masonry grid">
+
+
+
+
+        <article class="sea boat lighthouse people">
+            <img src="http://kamleshyadav.com/wp/fishing/wp-content/uploads/2016/07/port2-1.jpg"
+                class="img-responsive" />
+            <div class="overlay">
+                <div class="over-h">
+                    <h6>NETTING</h6>
+                    <hr>
+                    <a href="#"> VIEW PROJECT</a>
+                </div>
+            </div>
+        </article>
+        <article class="bird boat lighthouse">
+            <img src="http://kamleshyadav.com/wp/fishing/wp-content/uploads/2016/07/port3-1.jpg"
+                class="img-responsive" />
+            <div class="overlay">
+                <div class="over-h">
+                    <h6>NETTING</h6>
+                    <hr>
+                    <a href="#"> VIEW PROJECT</a>
+                </div>
+            </div>
+        </article>
+        <article class="bird boat lighthouse">
+            <img src="http://kamleshyadav.com/wp/fishing/wp-content/uploads/2016/07/port4-1.jpg"
+                class="img-responsive" />
+            <div class="overlay">
+                <div class="over-h">
+                    <h6>NETTING</h6>
+                    <hr>
+                    <a href="#"> VIEW PROJECT</a>
+                </div>
+            </div>
+        </article>
+
+        <article class="beach boat sea people">
+            <img src="http://kamleshyadav.com/wp/fishing/wp-content/uploads/2016/07/port5-1.jpg"
+                class="img-responsive" />
+            <div class="overlay">
+                <div class="over-h">
+                    <h6>NETTING</h6>
+                    <hr>
+                    <a href="#"> VIEW PROJECT</a>
+                </div>
+            </div>
+        </article>
+        <article class="beach people rock sea">
+            <img src="http://kamleshyadav.com/wp/fishing/wp-content/uploads/2016/07/port6-1.jpg"
+                class="img-responsive" />
+            <div class="overlay">
+                <div class="over-h">
+                    <h6>NETTING</h6>
+                    <hr>
+                    <a href="#"> VIEW PROJECT</a>
+                </div>
+            </div>
+
+        </article>
+        <article class="beach bird people">
+            <img src="http://kamleshyadav.com/wp/fishing/wp-content/uploads/2016/07/port7-1.jpg"
+                class="img-responsive" />
+            <div class="overlay">
+                <div class="over-h">
+                    <h6>NETTING</h6>
+                    <hr>
+                    <a href="#"> VIEW PROJECT</a>
+                </div>
+            </div>
+        </article>
+        <article class="beach boat lighthouse bird">
+            <img src="http://kamleshyadav.com/wp/fishing/wp-content/uploads/2016/07/port1-1.jpg"
+                class="img-responsive" />
+            <div class="overlay">
+                <div class="over-h">
+                    <h6>NETTING</h6>
+                    <hr>
+                    <a href="#"> VIEW PROJECT</a>
+                </div>
+            </div>
+        </article>
+        <article class="beach boat lighthouse">
+            <img src="http://kamleshyadav.com/wp/fishing/wp-content/uploads/2016/07/port8-1.jpg"
+                class="img-responsive" />
+            <div class="overlay">
+                <div class="over-h">
+                    <h6>NETTING</h6>
+                    <hr>
+                    <a href="#"> VIEW PROJECT</a>
+                </div>
+            </div>
+        </article>
+
+    </section>
+
+</div>
